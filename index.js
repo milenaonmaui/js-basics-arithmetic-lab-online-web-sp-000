ef571a2d5ef0152a3f5aa8086c4450e1a19ffec4
Milena Kari <milenakari@yahoo.com>
2019-08-14 18:58:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ let newID = 1000000000 + oldID;
 let ageIsValid = Number.isInteger(currentAge);
 let randomNumber = Math.random()*2;
 let randomInteger = Math.floor(randomNumber);
-let randomUserID = Math.random()*20 + 1000000001; 
+let randomUserID = Math.floor(Math.random()*20) + 1000000001; 
